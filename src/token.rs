ef7161556d855d6eb82982af4b5e1ddb8f816b6e
loganlang393 <logan.langewisch.393@my.csun.edu>
2025-03-27 13:32:55 -0700

    Started creating the structures for the tokens

--- src/token.rs
+++ src/token.rs
@@ -11,33 +11,130 @@
 //SEMICOLON
 //PRINT
 
-trait Token{}
+trait Token{
+	fn equals(t: Token, &self);
+	fn toString(&self);
+	fn hashCode(&self);
+}
 
-struct Identifier{}
-struct LParen{}
-struct RParen{}
-struct Plus{}
-struct Minus{}
-struct Star{}
-struct Div{}
-struct Equals{}
-struct Semicolon{}
-struct Print{}
+struct Identifier{
+	value: String,
+}
+struct LParen{
+	value: String,
+}
+struct RParen{
+	value: String,
+}
+struct Plus{
+	value: String,
+}
+struct Minus{
+	value: String,
+}
+struct Star{
+	value: String,
+}
+struct Div{
+	value: String,
+}
+struct Equals{
+	value: String,
+}
+struct Semicolon{
+	value: String,
+}
+struct Print{
+	value: String,
+}
 
-impl Token for Identifier{}
+impl Token for Identifier{
+	fn new(identifier: &str) -> Self{
+		Self {
+			value: identifier.to_string(),
+		}
+	}
+	
+        fn equals(t: Token, &self) -> bool{
+                return t == self;
+        }
 
-impl Token for LParen{}
+        fn toString(&self) -> String{
+                return self.identifier;
+        }
 
-impl Token for Rparen{}
+	fn hashCode(&self) -> u32{
+	}
+}
 
-impl Token for Plus{}
+impl Token for LParen{
+	fn new() -> Self{
+		Self {
+			value: "(",
+		}
+	}
+}
 
-impl Token for Star{}
+impl Token for Rparen{
+	fn new() -> Self{
+		Self{
+			value: ")",
+		}
+	}
+}
 
-impl Token for Div{}
+impl Token for Plus{
+        fn new() -> Self{
+                Self{
+                        value: "+",
+                }
+        }
+}
 
-impl Token for Equals{}
+impl Token for Minus{
+        fn new() -> Self{
+                Self{
+                        value: "-",
+                }
+        }
+}
 
-impl Token for Semicolon{}
+impl Token for Star{
+        fn new() -> Self{
+                Self{
+                        value: "*",
+                }
+        }
+}
 
-impl Token for Print{}
+impl Token for Div{
+        fn new() -> Self{
+                Self{
+                        value: "/",
+                }
+        }
+}
+
+impl Token for Equals{
+        fn new() -> Self{
+                Self{
+                        value: "=",
+                }
+        }
+}
+
+impl Token for Semicolon{
+        fn new() -> Self{
+                Self{
+                        value: ";",
+                }
+        }
+}
+
+impl Token for Print{
+        fn new() -> Self{
+                Self{
+                        value: "print",
+                }
+        }
+}
