f3150dc1af7ef354b2312bb38e5411724f5e46ef
loganlang393 <logan.langewisch.393@my.csun.edu>
2025-05-17 13:22:01 -0700

    added a fix that deallocates struct references that get generated in functions

--- src/output.c
+++ src/output.c
@@ -6,6 +6,11 @@ typedef struct MarkRefs MarkRefs;
 typedef struct Reference Reference;
 typedef struct Heap Heap;
 
+struct MarkRefs {
+	size_t object_size;
+	bool allocated;
+};
+
 struct Reference {
 	char* object_location;
 	size_t object_size;
@@ -19,21 +24,80 @@ struct Heap {
 	char* mid;
 	char* bump_pointer;
 	bool on_start;
-	size_t total_heap_size;
-	size_t current_heap_max;
-	size_t current_allocated_size;
+	char* total_heap_size;
+	char* current_heap_max;
 	Reference* entries;
 	size_t total_num_entries;
+	size_t num_entries;
 };
 
-Reference gc_allocate(struct Heap* h, size_t s, MarkRefs* mc) {
-	if (h->current_allocated_size + s <= h->current_heap_max) {
-		Reference entry = {h->bump_pointer, s, mc, true, true};
-		h->current_allocated_size += s;
-		h->bump_pointer += s;
-		return entry;
+void gc_reallocate(struct Heap* h) {
+	if(h->on_start) {
+		h->on_start = false;
+		h->bump_pointer = h->mid;
+	}else {
+		h->on_start = true;
+		h->bump_pointer = h->start;
 	}
-typedef struct test {
-} test;
+	Reference* current_list = h->entries;
+	h->entries = malloc(sizeof(Reference) * h->total_num_entries);
+	for(size_t i = 0; i < h->total_num_entries; i++) {
+		if(current_list[i]->mark) {
+			h->entries[h->num_entries] = *gc_allocate(h, current_list[i]->object_size, current_list[i]->children);
+			free(current_list[i]);
+			current_list[i] = NULL
+		}else {
+			free(current_list[i]);
+			current_list[i] = NULL;
+		}
+	}
+	free(current_list);
+	current_list = NULL
+}
 
-int x = /* unsupported expr */;
+Reference gc_allocate(struct Heap* h, size_t s, MarkRefs* mc) {
+	if(h->on_start) {
+		if (h->bump_pointer + s <= h->mid) {
+			if (h->num_entries == h->total_num_entries) {
+				gc_reallocate(&h);
+				if (h->num_entries == h->total_num_entries) {
+					Refernce entry = {h->bump_pointer, s, mc, false, false};
+					return entry;
+				} else {
+					return gc_allocate(&h, s, &mc);
+				}
+			}
+			Reference entry = {h->bump_pointer, s, mc, true, true};
+			h->entries[num_entries] = entry;
+			h->num_entries++;
+			h->bump_pointer += s;
+			return entry;
+		} else {
+			gc_reallocate(&h);
+			return gc_allocate(&h, s, &mc);
+		}
+	} else {
+		if (h->bump_pointer + s <= h->total_heap_size) {
+			if (h->num_entries == h->total_num_entries) {
+				gc_reallocate(&h);
+				if (h->num_entries == h-> total_num_entries) {
+					Reference entry = {h->bump_pointer, s, mc, false, false};
+					return NULL;
+				} else {
+					return gc_allocate(&h, s, &mc);
+				}
+			}
+			Reference entry = {h->bump_pointer, s, mc, true, true};
+			h->num_entries++;
+			h->bump_pointer += s;
+			return &entry;
+		} else {
+			gc_reallocate(&h);
+			return gc_allocate(&h, s, &mc);
+		}
+	}
+}
+void gc_deallocate(struct Heap* h, Reference reference) {
+	h->current_allocated_size -= reference->object_size;
+	reference->mark=false;
+}
